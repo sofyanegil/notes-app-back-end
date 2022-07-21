2a82f850502b34b4c8b18f5f954ae39b56813666
Sofyan Egi <sofyanegil@gmail.com>
2022-07-21 17:53:40 +0700

    feat: add refresh token service

---
+++ src/services/postgres/AuthenticationsService.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+class AuthenticationsService {
+  constructor() {
+    this._pool = new Pool();
+
+    this.addRefreshToken = this.addRefreshToken.bind(this);
+  }
+
+  async addRefreshToken(token) {
+    const query = {
+      text: 'INSERT INTO authentications VALUES ($1)',
+      values: [token],
+    };
+
+    await this._pool.query(query);
+  }
+}
+
+module.exports = AuthenticationsService;
