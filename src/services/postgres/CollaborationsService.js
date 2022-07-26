e826d48445ddd03ace591d9939e02efc02119113
Sofyan Egi <sofyanegil@gmail.com>
2022-07-26 21:04:55 +0700

    fix: typo deleteCollaboration service

--- src/services/postgres/CollaborationsService.js
+++ src/services/postgres/CollaborationsService.js
@@ -24,7 +24,7 @@ class CollaborationsService {
     return result.rows[0].id;
   }
 
-  async deleteCollaborator(noteId, userId) {
+  async deleteCollaboration(noteId, userId) {
     const query = {
       text: 'DELETE FROM collaborations WHERE note_id = $1 AND user_id = $2 RETURNING id',
       values: [noteId, userId],
